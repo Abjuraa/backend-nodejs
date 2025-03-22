9ddb12dbf1e646488b8769e245ce4ffc8bf7f86b
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-03-21 20:07:38 -0500

    cambio de origin en el cors
    se cambio la ruta 'http://localhost:3000' por '*' para utilizar la API desde un login externo de Flutter

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const createApp = () => {
     const app = express();
 
     app.use(cors({
-        origin: 'http://localhost:3000',
+        origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
         allowedHeaders: ['Content-Type', 'Authorization']
