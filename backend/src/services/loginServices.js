382d662ca57cb59fd88187ed8e1705f7ffadaba8
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-03-22 21:36:26 -0500

    validacion
    se hace la validación de la contraseña debe ser mayor a 8

--- backend/src/services/loginServices.js
+++ backend/src/services/loginServices.js
@@ -1,24 +1,29 @@
 const bcrypt = require('bcryptjs');
 const loginRepository = require('../repository/loginRepository');
 
-exports.registrar = async (usuario) => { 
+exports.registrar = async (usuario) => {
     const response = await loginRepository.registrar(usuario);
-    return response
+
+    if (usuario.password.length < 8) {
+        throw new Error('La contraseña debe tener al menos 8 caracteres')
+    } else {
+        return response
+    }
 }
 
 exports.ingresar = async (usuario) => {
     const datos = await loginRepository.ingresar(usuario);
 
-    if (!datos) { 
+    if (!datos) {
         throw new Error('El usuario no existe')
     }
 
     const match = await bcrypt.compare(usuario.password, datos.password);
-    if (!match) { 
+    if (!match) {
         throw new Error('Contraseña incorrecta')
     }
 
-    return { 
+    return {
         message: 'Ingreso exitoso',
         id: datos.id,
         nombre: datos.nombre,
@@ -26,7 +31,7 @@ exports.ingresar = async (usuario) => {
     }
 }
 
-exports.obtenerUsuarios = async () => { 
+exports.obtenerUsuarios = async () => {
     const response = await loginRepository.obtenerUsuarios();
     return response;
 }
